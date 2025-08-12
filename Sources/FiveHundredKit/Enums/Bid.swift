336054325c65f5a08e6919698c83b17b1d9791dc
Derek Chai <chderek9@gmail.com>
2025-08-12 20:33:57 +1200

    Add points property on Bid

--- Sources/FiveHundredKit/Enums/Bid.swift
+++ Sources/FiveHundredKit/Enums/Bid.swift
@@ -14,3 +14,33 @@ enum Bid {
     case noTrumps(Int)
     case standard(Int, Suit)
 }
+
+extension Bid {
+    var points: Int {
+        switch self {
+        case .pass:
+            return 0
+        case .misere:
+            return 250
+        case .openMisere:
+            return 500
+        case .noTrumps(let int):
+            return 120 + (int - 6) * 100
+        case .standard(let int, let suit):
+            let summand: Int
+            
+            switch suit {
+            case .spades:
+                summand = 40
+            case .clubs:
+                summand = 60
+            case .diamonds:
+                summand = 80
+            case .hearts:
+                summand = 100
+            }
+            
+            return summand + (int - 6) * 100
+        }
+    }
+}
