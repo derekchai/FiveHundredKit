ee03b5dbbda79687cec86e54d5f118080cb99c37
Derek Chai <chderek9@gmail.com>
2025-08-12 18:51:10 +1200

    Add unit tests

--- Tests/FiveHundredKitTests/FiveHundredKitTests.swift
+++ Tests/FiveHundredKitTests/FiveHundredKitTests.swift
@@ -2,5 +2,14 @@ import Testing
 @testable import FiveHundredKit
 
 @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let north = Player(name: "North")
+    let east = Player(name: "East")
+    let south = Player(name: "South")
+    let west = Player(name: "West")
+    
+    var state = FiveHundredState(players: [north, east, south, west])
+    
+    #expect(state.playerToPlay === north)
+    #expect(state.nextPlayer === east)
 }
+
---
+++ Tests/FiveHundredKitTests/FiveHundredStateTests.swift
@@ -0,0 +1,51 @@
+//
+//  FiveHundredStateTests.swift
+//  FiveHundredKit
+//
+//  Created by Derek Chai on 12/08/2025.
+//
+
+import Testing
+@testable import FiveHundredKit
+
+class FiveHundredStateTests {
+    let north, east, south, west: Player
+    var state: FiveHundredState
+    
+    init() {
+        north = Player(name: "North")
+        east = Player(name: "East")
+        south = Player(name: "South")
+        west = Player(name: "West")
+        
+        state = FiveHundredState(players: [north, east, south, west])
+    }
+    
+    @Test func testPlayerOrder() async throws {
+        #expect(state.playerToPlay === north)
+        #expect(state.nextPlayer === east)
+
+        state.play(.joker)
+        
+        #expect(state.playerToPlay === east)
+        #expect(state.nextPlayer === south)
+
+        state.play(.joker)
+
+        #expect(state.playerToPlay === south)
+        #expect(state.nextPlayer === west)
+        
+        state.play(.joker)
+        
+        #expect(state.playerToPlay === west)
+        #expect(state.nextPlayer === north)
+
+        state.play(.joker)
+        
+        #expect(state.playerToPlay === north)
+        #expect(state.nextPlayer === east)
+    }
+}
+
+
+
